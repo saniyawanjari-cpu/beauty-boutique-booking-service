e243c4bc182369b1d723e8d36e2ea1e6d261a9aa
Saniya Wanjari <saniyawanjari764@gmail.com>
2026-06-18 11:14:54 +0530

    Implemented user registration with BCrypt and PostgreSQL

--- src/main/java/com/saniya/bookingservice/controller/AuthController.java
+++ src/main/java/com/saniya/bookingservice/controller/AuthController.java
@@ -1,5 +1,9 @@
 package com.saniya.bookingservice.controller;
 
+import com.saniya.bookingservice.dto.RegisterRequest;
+import com.saniya.bookingservice.entity.User;
+import com.saniya.bookingservice.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import com.saniya.bookingservice.security.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -7,6 +11,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Autowired
     JwtService jwtService;
@@ -15,5 +24,28 @@ public class AuthController {
     public String login() {
 
         return jwtService.generateToken("saniya");
+    }
+    @PostMapping("/register")
+    public String register(
+            @RequestBody RegisterRequest request
+    ) {
+
+        User user = new User();
+
+        user.setUsername(
+                request.getUsername()
+        );
+
+        user.setPassword(
+                passwordEncoder.encode(
+                        request.getPassword()
+                )
+        );
+
+        user.setRole("CUSTOMER");
+
+        userRepository.save(user);
+
+        return "User Registered Successfully";
     }
 }
